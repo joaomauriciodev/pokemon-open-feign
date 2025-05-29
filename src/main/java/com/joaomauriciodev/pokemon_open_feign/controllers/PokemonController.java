13f534e6e5e4cf146a107e30f29e061973602864
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-29 10:40:26 -0300

    refactor: use service on controller

--- src/main/java/com/joaomauriciodev/pokemon_open_feign/controllers/PokemonController.java
+++ src/main/java/com/joaomauriciodev/pokemon_open_feign/controllers/PokemonController.java
@@ -2,25 +2,33 @@ package com.joaomauriciodev.pokemon_open_feign.controllers;
 
 import com.joaomauriciodev.pokemon_open_feign.client.PokemonClient;
 import com.joaomauriciodev.pokemon_open_feign.client.PokemonListResponse;
+import com.joaomauriciodev.pokemon_open_feign.services.PokemonService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PokemonController {
 
-    final PokemonClient pokemonClient;
+    final PokemonService pokemonService;
 
-    public PokemonController(PokemonClient pokemonClient) {
-        this.pokemonClient = pokemonClient;
+    public PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
     }
 
     @GetMapping("/pokemons")
     public ResponseEntity<PokemonListResponse> getAllPokemons(
-
+        @RequestParam(name = "offset", defaultValue = "0") int offset,
+        @RequestParam(name = "limit", defaultValue = "20") int limit
     ) {
-        return ResponseEntity.ok(pokemonClient.getAllPokemons());
+        try {
+            PokemonListResponse response = pokemonService.getPokemonList(offset, limit);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
