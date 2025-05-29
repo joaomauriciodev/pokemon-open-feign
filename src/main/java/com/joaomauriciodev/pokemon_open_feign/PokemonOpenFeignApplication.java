b45b2b80a43a6e555f90d47ad250bd540fd81108
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-29 10:04:00 -0300

    inital commit

---
+++ src/main/java/com/joaomauriciodev/pokemon_open_feign/PokemonOpenFeignApplication.java
@@ -0,0 +1,13 @@
+package com.joaomauriciodev.pokemon_open_feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonOpenFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonOpenFeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaomauriciodev/pokemon_open_feign/PokemonOpenFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomauriciodev.pokemon_open_feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonOpenFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
